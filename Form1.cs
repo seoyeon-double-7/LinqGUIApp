81b6ed31c0d2bce22f9a11f9998916e240a75b6e
Seoyeon Bae <s2103@e-mirim.hs.kr>
2023-08-08 11:32:28 +0900

    예제 데이터 작성

--- Form1.cs
+++ Form1.cs
@@ -12,6 +12,17 @@ namespace LinqGUIApp
 {
     public partial class Form1 : Form
     {
+        static private List<Product> products = new List<Product>()
+        {
+            new Product (){ Name="맥북", Price = 2000000},
+            new Product (){ Name="메모장", Price = 5000},
+            new Product (){ Name="샤프", Price = 3000},
+            new Product (){ Name="노트", Price = 6000},
+            new Product (){ Name="연필", Price = 1000},
+            new Product (){ Name="안경", Price = 150000},
+            new Product (){ Name="기타", Price = 60000},
+
+        };
         public Form1()
         {
             InitializeComponent();
